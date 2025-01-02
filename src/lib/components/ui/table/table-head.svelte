5ca51f8239df63b1407090314c451f86643c6233
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-01-02 16:07:58 +0530

    more UI shadcn components

--- src/lib/components/ui/table/table-head.svelte
+++ src/lib/components/ui/table/table-head.svelte
@@ -14,8 +14,8 @@
 <th
 	bind:this={ref}
 	class={cn(
-		"text-muted-foreground h-12 px-4 text-left align-middle font-medium [&:has([role=checkbox])]:pr-0",
-		className
+		"h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+		className,
 	)}
 	{...restProps}
 >
