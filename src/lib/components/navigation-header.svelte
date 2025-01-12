da362a444d91947fadc18f068e5c3433034967bb
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-01-12 21:41:48 +0530

    fix the thing

--- src/lib/components/navigation-header.svelte
+++ src/lib/components/navigation-header.svelte
@@ -51,7 +51,7 @@
 				{:else}
 					<Breadcrumb.Item class="hidden md:block">
 						<Breadcrumb.Link href="/{segments.slice(0, i + 1).join('/')}"
-							>{SEGMENT_TITLES[segment]}</Breadcrumb.Link
+							>{SEGMENT_TITLES[segment] ?? segment}</Breadcrumb.Link
 						>
 					</Breadcrumb.Item>
 					<Breadcrumb.Separator class="hidden md:block" />
