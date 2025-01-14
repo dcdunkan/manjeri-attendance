05f4edb9d50888badc593b9841ede9ec5c5cacb5
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-01-14 21:20:27 +0530

    fix: student info

--- src/routes/(authenticated)/(administrator)/admin/batches/[id]/students/[studentId]/+page.server.ts
+++ src/routes/(authenticated)/(administrator)/admin/batches/[id]/students/[studentId]/+page.server.ts
@@ -11,38 +11,6 @@ export const load: PageServerLoad = async (event) => {
 	}
 
 	return {
-		student: Promise.resolve({
-			id: 1001,
-			fullName: "Sreerag P P",
-			rollNumber: 1,
-			isRep: false,
-			account: { login: "2022001" },
-			enrollments: [
-				{
-					id: 1,
-					subject: { id: 13, name: "Medicine", periodCount: 0, asbentCount: 0 },
-				},
-				{
-					id: 2,
-					subject: { id: 14, name: "Microbiology", periodCount: 0, asbentCount: 0 },
-				},
-				{
-					id: 3,
-					subject: { id: 15, name: "Pathology", periodCount: 33, asbentCount: 2 },
-				},
-				{
-					id: 4,
-					subject: { id: 16, name: "Pharmacology", periodCount: 4, asbentCount: 0 },
-				},
-			],
-			representations: [
-				{
-					subjectId: 16,
-					id: 10,
-					studentId: 1001,
-				},
-			],
-			batch: { id: 10, name: "2022" },
-		}) satisfies ReturnType<typeof getStudent>, // getStudent(batchId, studentId),
+		student: getStudent(batchId, studentId),
 	};
 };
