cfd7697900d4751a95ae229bc8715229b0fde877
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-01-15 00:32:33 +0530

    Re-arrange things here and there

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,20 +3,7 @@
 	import { ModeWatcher } from "mode-watcher";
 	import { Toaster } from "$lib/components/ui/sonner";
 
-	import { onNavigate } from "$app/navigation";
-
 	let { children } = $props();
-
-	onNavigate((navigation) => {
-		if (!document.startViewTransition) return;
-
-		return new Promise((resolve) => {
-			document.startViewTransition(async () => {
-				resolve();
-				await navigation.complete;
-			});
-		});
-	});
 </script>
 
 <Toaster richColors toastOptions={{ class: "font-sans" }} />
